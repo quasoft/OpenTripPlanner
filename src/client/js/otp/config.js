5d7d37bacda86fb1f076acb970f34688367fb389
QuaSoft <info@quasoft.net>
2015-07-26 15:44:06 +0300

    Change name of default map layer

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -142,7 +142,7 @@ otp.config = {
         {
             id : 'planner',
             className : 'otp.modules.multimodal.MultimodalPlannerModule',
-            defaultBaseLayer : 'MapQuest OSM',
+            defaultBaseLayer : 'Стандартна карта',
             isDefault: true
         }/*,
         {
