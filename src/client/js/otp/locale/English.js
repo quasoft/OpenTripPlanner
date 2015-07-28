9a56256ead5f864b708d5d7e5ac43ce0b4283d10
QuaSoft <info@quasoft.net>
2015-07-28 09:08:00 +0300

    Updating translations

--- src/client/js/otp/locale/English.js
+++ src/client/js/otp/locale/English.js
@@ -2,14 +2,14 @@
    modify it under the terms of the GNU Lesser General Public License
    as published by the Free Software Foundation, either version 3 of
    the License, or (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 otp.namespace("otp.locale");
@@ -26,7 +26,7 @@ otp.locale.English = {
         name: 'English',
         //FALSE-imperial units are used
         //TRUE-Metric units are used
-        metric : false, 
+        metric : false,
         //Name of localization file (*.po file) in src/client/i18n
         locale_short : "en",
         //Name of datepicker localization in
@@ -56,11 +56,7 @@ otp.locale.English = {
                 title: 'About',
                 content: '<p>About this site</p>',
                 //cssClass: 'otp-contactWidget',
-            },
-            {
-                title: 'Contact',
-                content: '<p>Comments? Contact us at...</p>'
-            },
+            }
     ],
 
 
@@ -74,4 +70,3 @@ otp.locale.English = {
 
     CLASS_NAME : "otp.locale.English"
 };
-
